14e32a42a3b85609c6cf86881dab8a9530936c0d
RosannevanderPol <ccroos@gmail.com>
2015-11-05 18:41:48 +0100

    update

--- app/src/main/java/com/example/rosanne/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/rosanne/mrpotatohead/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.rosanne.mrpotatohead;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +12,92 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void selectItem(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (view.getId()){
+            case R.id.checkBox_hat:
+                ImageView hat = (ImageView) findViewById(R.id.image_hat);
+                if(checked){
+                    hat.setVisibility(View.VISIBLE);
+                }
+                else {
+                    hat.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_eyes:
+                ImageView eyes = (ImageView) findViewById(R.id.imageView_eyes);
+                if(checked){
+                    eyes.setVisibility(View.VISIBLE);
+                }
+                else{
+                    eyes.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_ears:
+                ImageView ears = (ImageView) findViewById(R.id.imageView_ears);
+                if(checked){
+                    ears.setVisibility(View.VISIBLE);
+                }
+                else{
+                    ears.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_arms:
+                ImageView arms = (ImageView) findViewById(R.id.imageView_arms);
+                if(checked){
+                    arms.setVisibility(View.VISIBLE);
+                }
+                else{
+                    arms.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_shoes:
+                ImageView shoes = (ImageView) findViewById(R.id.imageView_shoes);
+                if(checked){
+                    shoes.setVisibility(View.VISIBLE);
+                }
+                else {
+                    shoes.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_eyebrows:
+                ImageView eyebrows = (ImageView) findViewById(R.id.imageView_eyebrows);
+                if(checked){
+                    eyebrows.setVisibility(View.VISIBLE);
+                }
+                else {
+                    eyebrows.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_glasses:
+                ImageView glasses = (ImageView) findViewById(R.id.imageView_glasses);
+                if(checked){
+                    glasses.setVisibility(View.VISIBLE);
+                }
+                else {
+                    glasses.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_mustache:
+                ImageView mustache = (ImageView) findViewById(R.id.imageView_mustache);
+                if(checked){
+                    mustache.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mustache.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_mouth:
+                ImageView mouth = (ImageView) findViewById(R.id.imageView_mouth);
+                if(checked){
+                    mouth.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mouth.setVisibility(View.INVISIBLE);
+                }
+            case R.id.checkBox_nose:
+                ImageView nose = (ImageView) findViewById(R.id.imageView_nose);
+                if(checked){
+                    nose.setVisibility(View.VISIBLE);
+                }
+                else {
+                    nose.setVisibility(View.INVISIBLE);
+                }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+     }
 }
